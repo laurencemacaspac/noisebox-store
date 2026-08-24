32f5955ce9fdcd040f1698c297b8e1cd1b29f11e
Laurence Macaspac <laurencemacaspac@gmail.com>
2026-08-24 13:01:06 -0700

    Restore products table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { supabase } from "@/lib/supabase";
 
 export default async function Home() {
   const { data: products, error } = await supabase
-    .from("products_test")
+    .from("products")
     .select("*")
     .order("id", { ascending: true });
 
